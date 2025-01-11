bf4f14ccb8b85b68b61e0d2b20d33b854404edb7
amar2mail9 <amar2mail9@gmail.com>
2025-01-11 14:35:22 +0530

    PAge not foun 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout/Layout"; // Assuming you have a layout component
+
 import HomePage from "./components/Home/HomePage";
 import ContactUs from "./components/contactUs/ContactUs";
 import BlogPage from "./components/Blogpage/BlogPage";
 import CategoryPage from "./components/CategoryWiseBlog/CategoryPage";
 import BlogDetails from "./components/blogDetails/BlogDetails";
-import Error from "./components/error/Error";
+import Error from "./components/pagenot/Error";
 
 export default function App() {
   return (
@@ -18,7 +18,7 @@ export default function App() {
         <Route path="/category/:category" element={<CategoryPage />} />
         <Route path="/blog-page/:slug" element={<BlogDetails />} />
         
-        {/* Catch-all for error pages */}
+       
         <Route path="*" element={<Error/>} />
       </Routes>
     </BrowserRouter>
--- src/components/pagenot/Error.jsx
+++ src/components/pagenot/Error.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Layout from "../Layout/Layout";
-import { FiArrowDownLeft, FiArrowLeft } from "react-icons/fi";
+
+import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <Layout>
+    <>
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div className="text-center p-8 bg-white rounded-lg shadow-xl transform transition duration-500 ease-in-out scale-95 opacity-0 animate-fadeIn">
           <h1 className="text-6xl font-bold text-red-600 mb-4">404</h1>
@@ -16,7 +16,7 @@ export default function Error() {
             Please check the URL or go back to the homepage.
           </p>
           <div className="flex justify-center">
-            <Link to={'/'}>
+            <Link to={"/"}>
               <button className="flex text-sm items-center gap-3 bg-gray-500 mt-2  py-2 px-6 rounded-lg text-white">
                 <FiArrowLeft /> Back to Home
               </button>
@@ -24,6 +24,6 @@ export default function Error() {
           </div>
         </div>
       </div>
-    </Layout>
+    </>
   );
 }
